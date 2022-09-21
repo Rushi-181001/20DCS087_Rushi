5e8d854d8ad40732c5ada6bc6e076fb4adceb335
Rushi <patelmukti1806@gmail.com>
2022-09-21 10:59:03 -0700

    Updates

---
+++ precache-manifest.9f2ed3a48342739299409004495898fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17875e5881a341c2c57ad02751d05923",
+    "url": "/20DCS087_Rushi/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/20DCS087_Rushi/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "a53ffd74ef066e20eb83",
+    "url": "/20DCS087_Rushi/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/20DCS087_Rushi/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/20DCS087_Rushi/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53ffd74ef066e20eb83",
+    "url": "/20DCS087_Rushi/static/js/main.bd49b8ff.chunk.js"
+  },
+  {
+    "revision": "716339913fbcce90fd3c",
+    "url": "/20DCS087_Rushi/static/js/runtime-main.09112515.js"
+  }
+]);
